d1e93b7280b1407ec938a1672f9a479742f800a8
bishwas-py <bishwasbh@gmail.com>
2025-04-07 10:41:58 +0545

    cleanup

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 	"unsafe"
 )
@@ -23,13 +24,18 @@ const (
 )
 
 var (
-	user32   = syscall.MustLoadDLL("user32.dll")
-	setHook  = user32.MustFindProc("SetWindowsHookExW")
-	nextHook = user32.MustFindProc("CallNextHookEx")
-	getMsg   = user32.MustFindProc("GetMessageW")
-	keybd    = user32.MustFindProc("keybd_event")
-	getmod   = syscall.MustLoadDLL("kernel32.dll").MustFindProc("GetModuleHandleW")
-	winDown  bool
+	user32        = syscall.MustLoadDLL("user32.dll")
+	setHook       = user32.MustFindProc("SetWindowsHookExW")
+	nextHook      = user32.MustFindProc("CallNextHookEx")
+	getMsg        = user32.MustFindProc("GetMessageW")
+	keybd         = user32.MustFindProc("keybd_event")
+	unhookWinHook = user32.MustFindProc("UnhookWindowsHookEx")
+
+	kernel32 = syscall.MustLoadDLL("kernel32.dll")
+	getmod   = kernel32.MustFindProc("GetModuleHandleW")
+
+	winDown    bool
+	hookHandle uintptr
 )
 
 type kbd struct {
@@ -40,14 +46,16 @@ type kbd struct {
 	Info uintptr
 }
 
+// Callback function for keyboard hook
 func proc(code int, wp, lp uintptr) uintptr {
 	if code < 0 {
 		r, _, _ := nextHook.Call(0, uintptr(code), wp, lp)
 		return r
 	}
 
-	k := (*kbd)(unsafe.Pointer(lp))
+	k := (*kbd)(unsafe.Pointer(p))
 
+	// Optimize check order for most common case first
 	if wp == WM_KEYDOWN {
 		if k.Code == VK_LWIN || k.Code == VK_RWIN {
 			winDown = true
@@ -60,23 +68,56 @@ func proc(code int, wp, lp uintptr) uintptr {
 	} else if wp == WM_KEYUP && (k.Code == VK_LWIN || k.Code == VK_RWIN) {
 		winDown = false
 	}
+
 	r, _, _ := nextHook.Call(0, uintptr(code), wp, lp)
 	return r
 }
 
+// Cleanup resources when exiting
+func cleanup() {
+	if hookHandle != 0 {
+		unhookWinHook.Call(hookHandle)
+		hookHandle = 0
+	}
+
+	// Release DLLs to prevent resource leaks
+	user32.Release()
+	kernel32.Release()
+}
+
 func main() {
-	// Set up clean termination
+	// Lock this thread to the OS thread since it's handling the Windows message loop
+	runtime.LockOSThread()
+
+	// Ensure system can handle our background load
+	runtime.GOMAXPROCS(1) // This app doesn't need multiple cores
+
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() { <-c; os.Exit(0) }()
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanup()
+		os.Exit(0)
+	}()
+
+	defer cleanup() // Ensure cleanup on exit
 
 	// Install keyboard hook
 	mod, _, _ := getmod.Call(0)
-	setHook.Call(WH_KEYBOARD_LL, syscall.NewCallback(proc), mod, 0)
+	handle, _, _ := setHook.Call(WH_KEYBOARD_LL, syscall.NewCallback(proc), mod, 0)
+	if handle == 0 {
+		os.Exit(1) // Failed to set hook
+	}
+	hookHandle = handle
 
-	// Message loop
+	// Message loop - keep it minimal for startup use
 	var m struct{ Hwnd, Msg, Wp, Lp, Time, Pt uintptr }
 	for {
-		getMsg.Call(uintptr(unsafe.Pointer(&m)), 0, 0, 0)
+		// Minimize CPU usage in message loop
+		ret, _, _ := getMsg.Call(uintptr(unsafe.Pointer(&m)), 0, 0, 0)
+		if ret == 0 {
+			// WM_QUIT received
+			break
+		}
 	}
 }
